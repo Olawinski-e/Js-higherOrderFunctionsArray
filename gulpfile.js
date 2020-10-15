00845b4e30bf72d1d26886f97a38282067363882
Estelle OLAWINSKI <estelle@fr.mpitech.com>
2020-10-15 15:35:39 +0200

    forEach filter map sort reduce

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var reload = browserSync.reload;
+
+gulp.task("serve", function () {
+  browserSync.init({
+    server: {
+      baseDir: "app",
+    },
+  });
+});
+
+gulp.watch("app/*.html").on("change", reload);
+gulp.watch("app/*.js").on("change", reload);
